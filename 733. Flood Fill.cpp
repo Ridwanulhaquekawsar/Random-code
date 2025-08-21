163fbcaf1ba01eb119c047c8c0a402720bbd6c6e
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-08-21 11:59:20 +0600

    Update 733. Flood Fill.cpp

--- 733. Flood Fill.cpp
+++ 733. Flood Fill.cpp
@@ -13,3 +13,20 @@ public:
         image1=&image; old_clr=image[sr][sc]; new_clr=color; n=image.size(); m=image[0].size(); if(old_clr==new_clr){return image;} dfs(sr,sc); return image;
     }
 };
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+public:
+    int n,m,val,ci,cj; bool vis[55][55]; vector<pair<int,int>>mov={{0,1},{0,-1},{-1,0},{1,0}}; bool isvalid(int i,int j){return i>=0 && i<n && j>=0 && j<m;} 
+
+    void dfs(vector<vector<int>>& image, int pi, int pj, int newcolor){
+        image[pi][pj]=newcolor; for(auto mv : mov){ci=pi+mv.first; cj=pj+mv.second; if(isvalid(ci,cj) && !vis[ci][cj] && image[ci][cj]==val){vis[pi][pj]=true; dfs(image,ci,cj,newcolor);}}
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newcolor) {
+        n=image.size(); m=image[0].size(); val=image[sr][sc]; dfs(image,sr,sc,newcolor); return image;
+    }
+};
