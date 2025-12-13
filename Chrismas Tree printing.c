88fa29fb5315ce43ae56d34c8c01db31ba025cee
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-12-13 21:26:55 +0600

    Update Chrismas Tree printing.c

--- Chrismas Tree printing.c
+++ Chrismas Tree printing.c
@@ -1,24 +1,24 @@
-
-
 #include <stdio.h>
 int n, space, star, space1,extra;
 
 void print_tree(){
     scanf("%d", &n);
         extra = (n-1)/2;
-        for (int i = 1; i <= 6+5+extra; i++)
-        {
-            if (i<=6+extra)
-            {
+    
+        for (int i = 1; i <= 6+5+extra; i++){
+            if (i<=6+extra){
                 space = 6-i+extra; star = 2*i-1; 
                 if (n==star) space1 = 6-i+extra;
+                
                 for (int j = 1; j <= space; j++) printf(" ");
                 for (int j = 1; j <= star; j++)printf("*");
+                
                 printf("\n");
-            }else
-            {
+            }
+            else{
                 for (int j = 1; j <= space1; j++) printf(" ");
                 for (int j = 1; j <= n; j++) printf("*");
+                
                 printf("\n"); 
             }
         } 
